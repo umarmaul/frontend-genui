ec316c9cadf28af81dc652305657418bfc21d82e
umarmaul <122956420+umarmaul@users.noreply.github.com.>
2024-11-21 17:35:18 +0700

    tambahan form survey

--- components/floating-contact-btn.tsx
+++ components/floating-contact-btn.tsx
@@ -7,14 +7,14 @@ export function FloatingContactBtn() {
     return (
         <Link
             className="fixed md:bottom-5 bottom-3 right-5 z-50 flex items-center"
-            href={"https://wa.me/6285141739229"}
+            href={"https://forms.gle/FGqP8CbezznY8BWQ8"}
         >
             <div className="relative">
-                <button className="rounded-full border-2 border-white bg-green-700 px-4 md:text-lg text-xs font-bold text-white hover:bg-green-600">
-                    <div className="pr-8">Klik untuk tanya-tanya</div>
+                <button className="rounded-full border-2 border-white bg-purple-700 px-4 md:text-lg text-xs font-bold text-white hover:bg-purple-600">
+                    <div className="pr-8">Klik untuk isi survey</div>
                 </button>
                 <Image
-                    src={"logo_wa_trans.svg"}
+                    src={"/google-form-icon.png"}
                     alt="WhatsApp Logo"
                     width={40} // Set the desired width
                     height={40} // Set the desired height
