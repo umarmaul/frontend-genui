bf5c43f2034b3ba0a89eba0c9de86bd6f0a52806
umarmaul <122956420+umarmaul@users.noreply.github.com.>
2024-11-24 21:01:11 +0700

    update to public

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,14 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const poolConfig = {
-    host: "145.223.117.210",
-    port: 6543,
-    user: "postgres",
-    password:
-        "Ep99DiEJ05RxqEjtgHPVG9O1JSIGDSvXgEMgv4x3OWgIl9XuMN9vHJo7FeIJD623",
-    database: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 };
 
 export const pool = new pg.Pool(poolConfig);
